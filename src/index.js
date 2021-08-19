7b15ad5dc167b779dcfc1f58af6f189d21cf9e05
agusm13 <agus.martinez.o@gmail.com>
2021-08-19 16:36:07 -0700

    css, forecast
    changes to css, trying to get weather and forecast to work

--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ function currentTemp(response) {
 function getForecast(coordinates) {
   let apiKey = "0fbc736f2359584075fc6a76570cf171";
   let apiUrlForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrlForecast).then(displayForecast);
+  axios.get(apiUrlForecast).then(currentTemp);
 }
 
 navigator.geolocation.getCurrentPosition(getForecast);
